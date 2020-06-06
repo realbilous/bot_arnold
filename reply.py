51aadde425301e113c598637f7268240964ba43e
Vlad Bilous <bilousv.ua@gmail.com>
2020-06-06 21:37:31 +0200

    Making a reply, and reading fetched data into dict

---
+++ reply.py
@@ -0,0 +1,51 @@
+import csv
+
+# Country attributes
+headers = ["Capital", "Population", "Official languages", "GDP", "Flag"]
+
+def make_reply(msg, countries_dict):
+    """
+        Makes reply based on a message received 
+
+        Parameters: 
+            msg (str): message received from a user
+            countries_dict (dict): dictionary with data data for every country
+
+        Returns: 
+            str: A string with generated reply to a user
+            str: A string with url to an image
+
+    """
+    reply = 'Not found.'
+    img_url = None
+    if msg in countries_dict.keys():
+        reply = "Country: " + msg
+        for i in range(len(headers) - 1): 
+            # Write a name of an attribute and corresponding data 
+            reply += "\n" + headers[i] +": " + ", ".join(countries_dict[msg][i].split(";"))
+        img_url = countries_dict[msg][-1] #URL to image of a flag on Wikipedia
+
+    return reply, img_url
+
+def read_data():
+    '''
+    Read all data from csv to dictionary
+    
+    Returns dictionaty with data for all countries 
+    where country names are keys
+    '''
+    data_dict = {}
+    with open("data/countries.csv", encoding="utf-8") as data_file:
+        csv_data = csv.reader(data_file)
+
+        next(csv_data) #Skip headers
+    
+        for row in csv_data:
+            # Names of country is a key
+            # Other data about country is stored in a list
+            # [Capital, Population, Off. languages, GDP, Flag]
+            data_dict[row[0]] = [entry for entry in row[1:]] 
+    return data_dict
+
+
+
